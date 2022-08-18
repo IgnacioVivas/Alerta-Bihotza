eff0e80cf3d9cbcfd2d484e0b7f417bcfb258851
IgnacioVivas <ignacab@live.com>
2022-08-18 12:52:18 -0300

    fix in chefckout cotainer

--- src/components/Collection.jsx
+++ src/components/Collection.jsx
@@ -1,30 +1,42 @@
-import React from 'react'
-import imgSpring from '../assets/images/home-image.jpg'
-import imgSummer from '../assets/images/home-image2.jpg'
+import React from "react";
+import imgSpring from "../assets/images/home-image.jpg";
+import imgSummer from "../assets/images/home-image2.jpg";
 
 function Collection() {
-    return (
-        <div className="container-fluid">
-            <div className="row">
-                <div className="cont-spring col-lg-6" style={{backgroundImage: `url(${imgSpring})`}}>
-                    <h2>SPRING COLLECTION</h2>
-                    <div className="raya"></div>
-                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</h3>
-                    <button>
-                        <a href="">SHOP NOW</a>
-                    </button>
-                </div>
-                <div className="cont-summer col-lg-6" style={{backgroundImage: `url(${imgSummer})`}}>
-                    <h2>SUMMER COLLECTION</h2>
-                    <div className="raya"></div>
-                    <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</h3>
-                    <button>
-                        <a href="">SHOP NOW</a>
-                    </button>
-                </div>
-            </div>
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div
+          className="cont-spring col-lg-6"
+          style={{ backgroundImage: `url(${imgSpring})` }}
+        >
+          <h2>SPRING COLLECTION</h2>
+          <div className="raya"></div>
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore.
+          </h3>
+          <button>
+            <a>SHOP NOW</a>
+          </button>
         </div>
-    )
+        <div
+          className="cont-summer col-lg-6"
+          style={{ backgroundImage: `url(${imgSummer})` }}
+        >
+          <h2>SUMMER COLLECTION</h2>
+          <div className="raya"></div>
+          <h3>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore.
+          </h3>
+          <button>
+            <a>SHOP NOW</a>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Collection
+export default Collection;
--- src/components/ItemDetailConteiner.jsx
+++ src/components/ItemDetailConteiner.jsx
@@ -1,45 +1,36 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-import ItemDetail from './ItemDetail';
-import { useParams } from 'react-router-dom';
-import { getFirestore } from '../service/GetFireBase';
-import Spinner from 'react-bootstrap/Spinner'
-
-
+import React from "react";
+import { useState, useEffect } from "react";
+import ItemDetail from "./ItemDetail";
+import { useParams } from "react-router-dom";
+import { getFirestore } from "../service/GetFireBase";
+import Spinner from "react-bootstrap/Spinner";
 
 function ItemDetailConteiner() {
+  const { id } = useParams();
+  const [product, setProduct] = useState({});
+  const [loanding, setLoanding] = useState(true);
 
-    const { id } = useParams();
-    const [product, setProduct] = useState({})
-    const [loanding, setLoanding] = useState(true)
+  useEffect(() => {
+    const baseDeDatos = getFirestore();
+    const itemCollection = baseDeDatos.collection("Items");
+    const item = itemCollection.doc(id);
+    item.get().then((resp) => {
+      if (!resp.exists) {
+        return;
+      }
+      setProduct({ id: resp && resp.id, ...resp.data() });
+      setLoanding(false);
+    });
+  }, []);
 
-    useEffect(() => {
-
-        const baseDeDatos = getFirestore();
-        const itemCollection = baseDeDatos.collection("Items");
-        const item = itemCollection.doc(id)
-        item.get().then((resp) => {
-            if (!resp.exists) {
-                return
-            }
-            setProduct({ id: resp.id, ...resp.data() })
-            setLoanding(false)
-
-        })
-
-    }, [])
-
-    return (
-        <>
-
-            {loanding ?
-                <Spinner animation="grow" variant="dark" />
-                :
-
-                <ItemDetail producto={product}></ItemDetail>
-            }
-
-        </>
-    )
+  return (
+    <>
+      {loanding ? (
+        <Spinner animation="grow" variant="dark" />
+      ) : (
+        <ItemDetail producto={product}></ItemDetail>
+      )}
+    </>
+  );
 }
-export default ItemDetailConteiner
+export default ItemDetailConteiner;
